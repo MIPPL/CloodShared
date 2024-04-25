c91729cfaf19a94488999c5e4e1f94d5a68fb6a8
MIP <mip.putalocura@gmail.com>
2024-04-25 20:14:38 +0200

    check password strength and confirmation

--- src/userauth/dto/CreateUser.ts
+++ src/userauth/dto/CreateUser.ts
@@ -1,10 +1,18 @@
-import { IsString, IsNotEmpty } from "class-validator";
+import { IsString, IsNotEmpty, IsStrongPassword, IsStrongPasswordOptions, ValidateIf } from "class-validator";
 import { UserBaseRecord } from "./BaseUser";
 
+const strongPasswordOptions = { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1} as IsStrongPasswordOptions;
+
 class CreateUserRequestDto extends UserBaseRecord {
+  @IsStrongPassword(strongPasswordOptions)
   @IsNotEmpty()
   @IsString()
   password: string;
+
+  @ValidateIf((o) => o.password === o.confirm_password)
+  @IsNotEmpty()
+  @IsString()
+  confirm_password: string;
 }
 
 class CreateUserResponseDto {
